1faca4ed57be735194d83e3de7389cbf11168058
Gardene Costa <101737142+Gardene10@users.noreply.github.com>
2022-08-01 15:54:51 -0300

    Add files via upload

---
+++ exe1.js
@@ -0,0 +1,11 @@
+
+var vel = 80
+
+console.log(`À velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60){ // condiçao simples
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('dirija sempre usando o cinto de segurança')
+
